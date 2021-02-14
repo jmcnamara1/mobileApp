f455f53e997f2a2ec69bd796d5bf6de56e44a746
Joel McNamara <jmmcnamara@deloitte.co.uk>
2021-02-14 16:48:35 +0000

    Added test running to jenkins build

--- src/test/java/com/appjoelblog/app/ws/MobileAppWsApplicationTests.java
+++ src/test/java/com/appjoelblog/app/ws/MobileAppWsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.appjoelblog.app.ws;
 
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MobileAppWsApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(MobileAppWsApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("the test has run");
 	}
 
 }
